490545ead78a5678980f9ab7619a27a27b3b9720
7bubba10 <aidan.decina1@marist.edu>
2026-03-22 11:46:48 -0400

    Added Login Page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import RegisterPage from './pages/RegisterPage';
+import LoginPage from './pages/LoginPage';
 
 function App() {
   return (
@@ -9,7 +10,7 @@ function App() {
       <Router>
         <Routes>
           <Route path="/" element={<div>Home</div>} />
-          <Route path="/login" element={<div>Login</div>} />
+          <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/dashboard" element={<div>Dashboard</div>} />
           <Route path="/results" element={<div>Results</div>} />
